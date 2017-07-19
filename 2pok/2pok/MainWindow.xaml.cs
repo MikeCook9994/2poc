537b38cd6ac5893ae9204d0419465a37c8eb5497
MikeCook9994 <mjcook3@wisc.edu>
2017-07-18 20:03:00 -0500

    updated binary

--- 2pok/2pok/MainWindow.xaml.cs
+++ 2pok/2pok/MainWindow.xaml.cs
@@ -2,6 +2,7 @@
 using System.Net;
 using System.Windows;
 
+using Microsoft.Practices.Unity;
 using WindowsInput;
 
 using _2pok.interfaces;
@@ -14,11 +15,36 @@ namespace _2pok
     public partial class MainWindow : Window
     {
         /// <summary>
+        /// True if this application is functioning as the host; false if otherwise.
+        /// </summary>
+        bool isHost = false;
+
+        /// <summary>
+        /// Used to set hooks for monitoring keyboard inputs on the client's machine.
+        /// </summary>
+        KeyboardMonitor keyboardMonitor;
+
+        /// <summary>
+        /// Used to set hooks for monitoring mouse inputs on the client's machine.
+        /// </summary>
+        MouseMonitor mouseMonitor;
+
+        /// <summary>
+        /// Provides an interface for receiving <see cref="Input"/> input events over the network.
+        /// </summary>
+        IInputHost inputHost;
+
+        /// <summary>
+        /// Provides an interface for sending <see cref="Input"/> input events over the network.
+        /// </summary>
+        IInputClient inputClient;
+
+        /// <summary>
         /// Initializes the Main Window and it's components.
         /// </summary>
         public MainWindow()
         {
-            InitializeComponent();
+            InitializeComponent(); 
         }
 
         /// <summary>
@@ -28,7 +54,16 @@ namespace _2pok
         /// <param name="e">Unused.</param>
         private void Window_Closing(object sender, System.ComponentModel.CancelEventArgs e)
         {
-            
+            if(this.isHost == false)
+            {
+                this.keyboardMonitor.Dispose();
+                this.mouseMonitor.Dispose();
+                this.inputClient.Close();
+            }
+            else
+            {
+                this.inputHost.Close();
+            }
         }
 
         /// <summary>
@@ -39,6 +74,8 @@ namespace _2pok
         /// <param name="e">Unused.</param>
         private void OpenConnection_Click(object sender, RoutedEventArgs e)
         {
+            this.isHost = true;
+
             //Disables the connection buttons so a second connection cannot attempt to be established.
             OpenConnection.IsEnabled = false;
             Connect.IsEnabled = false;
@@ -47,12 +84,12 @@ namespace _2pok
             // to forward data over the port to the machine hosting the application.
             int portNumber = Int32.Parse(Host_Port_Number_Textbox.Text);
 
-            IInputHost inputReceiver = new InputHost(portNumber);
+            this.inputHost = new InputHost(portNumber);
        
             IInputSimulator inputSimulator = new InputSimulator();
 
-            IKeyboard virtualKeyboard = new VirtualKeyboard(inputReceiver, inputSimulator, this);
-            IMouse virtualMouse = new VirtualMouse(inputReceiver, inputSimulator, this);
+            IKeyboard virtualKeyboard = new VirtualKeyboard(this.inputHost, inputSimulator, this);
+            IMouse virtualMouse = new VirtualMouse(this.inputHost, inputSimulator, this);
         }
 
         /// <summary>
@@ -63,6 +100,8 @@ namespace _2pok
         /// <param name="e">Unused.</param>
         private void Connect_Click(object sender, RoutedEventArgs e)
         {
+            this.isHost = false;
+
             //Disables the connection buttons so a second connection cannot attempt to be established.
             OpenConnection.IsEnabled = false;
             Connect.IsEnabled = false;
@@ -73,14 +112,14 @@ namespace _2pok
             IPAddress ipAddress = IPAddress.Parse(splitConnectionDetails[0]);
             int portNumber = Int32.Parse(splitConnectionDetails[1]);
 
-            IInputClient inputSender = new InputClient();
-            inputSender.Connect(ipAddress, portNumber);
+            this.inputClient = new InputClient();
+            this.inputClient.Connect(ipAddress, portNumber);
 
-            KeyboardMonitor keyboardMonitor = new KeyboardMonitor(true);
-            IKeyboard networkKeyboard = new NetworkKeyboard(inputSender, keyboardMonitor);
+            this.keyboardMonitor = new KeyboardMonitor(true);
+            IKeyboard networkKeyboard = new NetworkKeyboard(this.inputClient, this.keyboardMonitor);
 
-            MouseMonitor mouseMonitor = new MouseMonitor(true);
-            IMouse networkMouse = new NetworkMouse(inputSender, mouseMonitor);
+            this.mouseMonitor = new MouseMonitor(true);
+            IMouse networkMouse = new NetworkMouse(this.inputClient, this.mouseMonitor);
 
             System.Windows.Forms.Application.Run();
         }
